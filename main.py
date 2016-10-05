1) small fixes with saving results 2) added calculation t-value for mean 200..500. Troubles with visualisation

main.py
@@ -4,9 +4,6 @@ from os.path import join
 from mne.time_frequency import cwt_morlet
 import numpy as np
 from scipy.stats import ttest_ind
-
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 import os
@@ -21,7 +18,7 @@ def tft_transofrm(source):
     window_end = window_start+400
 
 
-    freqs = range(10,100,1)
+    freqs = range(10,12,1)
     sfreq = 1000
     res = np.zeros((source.shape[0],source.shape[1],len(freqs),source.shape[2]),dtype=np.float32)
     for i in xrange(source.shape[0]):
@@ -42,41 +39,53 @@ def calc_t_stat(target_data, nontarget_data):
     res = ttest_ind(target_data,nontarget_data,axis=0,equal_var=False)
     return res
 
-def visualise(data,title):
+def vis_matrix(data,title,x_label='Time',y_label='Channel'):
     #Plot 2D data as a image
     fig = plt.figure()
     plt.title(title)
     plt.imshow(data,aspect='auto')
     plt.colorbar()
-    plt.xlabel('Time')
-    plt.ylabel('Channel')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    axes = plt.gca()
+    axes.set_xlim([0,data.shape[1]-1])
+    axes.set_ylim([0,data.shape[0]-1])
     return fig
 
-def vis_each_freq(data,title):
+def vis_each_freq(data,title,res_path):
     # visualise data for each frequency and save it as a file
     # data in format channel x freq x time
-
-    if not os.path.isdir('results'):
-        os.mkdir('results')
-    if not os.path.isdir(os.path.join('results',title)):
-        os.mkdir(os.path.join('results',title))
+    image_path = os.path.join(res_path,title)
+    if not os.path.isdir(image_path):
+        os.mkdir(image_path)
     else:
-        file_names = [os.path.join('results',title,file_name) for file_name in os.listdir(os.path.join('results',title))]
+        file_names = [os.path.join(image_path,file_name) for file_name in os.listdir(image_path)]
         map(os.remove,file_names)
 
     for fq in range(data.shape[1]):
-        fig=visualise(data[:,fq,:],'%s fq=%f' %(title,(fq+10)))
-        plt.savefig(os.path.join('results',title,'_fq=%0.1f.png' % (fq+10)))
+        fig=vis_matrix(data[:,fq,:],'%s fq=%f' %(title,(fq+10)))
+        plt.savefig(os.path.join(image_path,'fq=%0.1f.png' % (fq+10)))
         plt.close(fig)
 
-def save_mat(data,title):
-    if not os.path.isdir('results'):
-        os.mkdir('results')
-    savemat(file_name = os.path.join('results',title),mdict=dict(title=data))
 
-def calc_metricts(data_path,sensor_type):
+def save_results(data,title,exp_num,need_image=True):
+    res_path = 'results'
+    if not os.path.isdir(res_path):
+        os.mkdir(res_path)
+
+    res_path = os.path.join(res_path,exp_num)
+    if not os.path.isdir(res_path):
+        os.mkdir(res_path)
+    savemat(file_name = os.path.join(res_path,title),mdict=dict(data=data)) #save data as .mat file with 'data' variable inside [channelas x freqs x times]
+
+    if need_image:
+        vis_each_freq(data,title,res_path) #save data as images in image_path folders
+
+def calc_metricts(data_path,exp_num,sensor_type):
     #Loading data
     #data start time = -820
+
+    data_path = join('..', 'meg_data1',exp_num)
     target_data, nontarget_data = get_data(data_path,sensor_type) #trials x channels x times
     sensor_type = sensor_type.split(' ')[-1]
 
@@ -88,18 +97,26 @@ def calc_metricts(data_path,sensor_type):
     # Calc mean for UNCORRECTED data
     third_target = first_target.mean(axis=0)
     third_nontarget = first_nontarget.mean(axis=0)
-    save_mat(third_target,'third_target_%s' %sensor_type)
-    save_mat(third_nontarget,'third_nontarget_%s' %sensor_type)
-    vis_each_freq(third_target,'mean_target_notcorrected_%s' %sensor_type)
-    vis_each_freq(third_nontarget,'mean_nontarget_notcorrected_%s' %sensor_type)
+    save_results(third_target,'third_target_%s' %sensor_type,exp_num)
+    save_results(third_nontarget,'third_nontarget_%s' %sensor_type,exp_num)
     del third_target,third_nontarget
 
 
     # Calc t-stat for UNCORRECTED data
     fivth = ttest_ind(first_target,first_nontarget,axis=0,equal_var=False)
-    vis_each_freq(fivth.statistic,'t-stat_notcorrected_%s' %sensor_type)
-    save_mat(fivth.statistic,'fivth_%s' %sensor_type)
+    save_results(fivth.statistic,'fivth_%s' %sensor_type,exp_num)
     del fivth
+
+    # Calc avaraget t-stats for mean value of interval [200:500]ms
+    start_window = 820+200
+    end_window = 820+500
+    seventh = ttest_ind(first_target[:,:,:,start_window:end_window].mean(axis=3),first_nontarget[:,:,:,start_window:end_window].mean(axis=3),axis=0,equal_var=False)
+    save_results(seventh.statistic,'seventh_%s' %sensor_type,exp_num,need_image=False)
+    title = 'T-stat_mean_200_500ms'
+    fig = vis_matrix(seventh.statistic,title,x_label='Channel',y_label='Frequency')
+    plt.savefig(os.path.join('results',exp_num,title+'.png'))
+    plt.close(fig)
+    del seventh
 
     #CORRECT data
     second_target = baseline_correction(first_target)
@@ -110,22 +127,26 @@ def calc_metricts(data_path,sensor_type):
     # Calc mean for CORRECTED data
     fourth_target = second_target.mean(axis=0)
     fourth_nontarget = second_nontarget.mean(axis=0)
-    save_mat(fourth_target,'fourth_target_%s' %sensor_type)
-    save_mat(fourth_nontarget,'fourth_nontarget_%s' %sensor_type)
-    vis_each_freq(fourth_target,'mean_target_corrected_%s' %sensor_type)
-    vis_each_freq(fourth_nontarget,'mean_nontarget_corrected_%s' %sensor_type)
+    save_results(fourth_target,'fourth_target_%s' %sensor_type,exp_num)
     del fourth_target,fourth_nontarget
 
     # Calc t-stat for CORRECTED data
     sixth = ttest_ind(second_target,second_nontarget,axis=0,equal_var=False)
-    vis_each_freq(sixth.statistic,'t-stat_corrected_%s' %sensor_type)
-    save_mat(sixth.statistic,'sixth_%s' %sensor_type)
+    save_results(sixth.statistic,'sixth_%s' %sensor_type,exp_num)
     del sixth
+
+def erase_dir(path):
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
 
 if __name__=='__main__':
     exp_num=sys.argv[1]
-    path = join('..', 'meg_data1',exp_num)
+    erase_dir(join('results',exp_num))
 
-    calc_metricts(path,'MEG GRAD')
+    path = join('..', 'meg_data1')
 
-    calc_metricts(path,'MEG MAG')
+    calc_metricts(path,exp_num,'MEG GRAD')
+    calc_metricts(path,exp_num,'MEG MAG')