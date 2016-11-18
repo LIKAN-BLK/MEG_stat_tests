45e9ce6260dee1291c5ee2e21dc783eb2bcf16a9
bogdan <kozyrskiy@gmail.com>
2016-11-18 15:53:47 +0300

    Working fix-vs-nonfix

--- main.py
+++ main.py
@@ -65,105 +65,47 @@ def vis_space_freq(data,title,freqs):
     axes.set_ylim([0,data.shape[0]-1])
     return fig
 
-def vis_each_freq(data,title,res_path):
-    # visualise data for each frequency and save it as a file
-    # data in format channel x freq x time
-    image_path = os.path.join(res_path,title)
-    if not os.path.isdir(image_path):
-        os.mkdir(image_path)
-    else:
-        file_names = [os.path.join(image_path,file_name) for file_name in os.listdir(image_path)]
-        map(os.remove,file_names)
 
-    for fq in range(data.shape[1]):
-        fig=vis_space_time(data[:,fq,:],'%s fq=%f' %(title,(fq+10)))
-        plt.savefig(os.path.join(image_path,'fq=%0.1f.png' % (fq+10)))
-        plt.close(fig)
-
-
-def save_results(data,title,result_path,need_image=True):
+def save_results(data,title,result_path):
     if not os.path.isdir(result_path):
         os.makedirs(result_path)
     savemat(file_name = os.path.join(result_path,title),mdict=dict(data=data)) #save data as .mat file with 'data' variable inside [channelas x freqs x times]
-
-    if need_image:
-        vis_each_freq(data,title,result_path) #save data as images in image_path folders
-
-def save_large_data(data,path_to_dir,freqs):
-    # @data have to be  [trials x channels x freqs x times]
-    os.mkdir(path_to_dir)
-    for fq in range(len(freqs)):
-        savemat(file_name = os.path.join(path_to_dir,'%d' %freqs[fq]),mdict=dict(data=data[:,:,fq,:]))
-
-def read_large_data(path_to_dir):
-    files = os.listdir(path_to_dir)
-    file = files[0]
-    files = files[1:]
-    data_sample = loadmat(os.path.join(path_to_dir,file))
-    res = np.empty((data_sample.shape[0],data_sample.shape[1],0,data_sample.shape[3]),np.float32)
-    for file in files:
-        res = np.concatenate(res,loadmat(os.path.join(path_to_dir,file))[:,:,np.newaxis,:],axis=2)
-    return res
-
-def freq_bands_mean_amplitude(data,band_width=5):
-    base_fq_indexes = range(0,data.shape[2],band_width)
-    res = np.empty((data.shape[0],data.shape[1],0,data.shape[3]),np.float32)
-    for fq in base_fq_indexes:
-        res = np.concatenate((res,data[:,:,fq:fq+band_width,:].mean(axis=2)[:,:,np.newaxis,:]),axis=2)
-    return res,base_fq_indexes
-
-def statistic_for_band_averaged_data(data_target,data_nontarget,epoch_start_time,sensor_type):
-    data_target_tmp,base_fq_indexes = freq_bands_mean_amplitude(data_target,band_width=5)
-    data_nontarget_tmp,_ = freq_bands_mean_amplitude(data_nontarget,band_width=5)
-
-    start_window = 200 - epoch_start_time
-    end_window = 500 - epoch_start_time
-    seventh = ttest_ind(data_target_tmp[:,:,:,start_window:end_window].mean(axis=3),data_nontarget_tmp[:,:,:,start_window:end_window].mean(axis=3),axis=0,equal_var=True)
-    del data_target_tmp, data_nontarget_tmp
-    save_results(seventh.statistic,'seventh_t_%s' %sensor_type,result_path,need_image=False)
-    save_results(seventh.pvalue,'seventh_p_%s' %sensor_type,result_path,need_image=False)
-    title = 'T-stat_mean_200_500ms_uncorrected'
-    fig = vis_space_freq(seventh.statistic,title,[freqs[fq_ind] for fq_ind in base_fq_indexes])
-    plt.savefig(os.path.join(result_path,title+'_'+sensor_type+'.png'))
+    fig = vis_space_freq(data, title, freqs)
+    plt.savefig(os.path.join(result_path, title  + '.png'))
     plt.close(fig)
-    heads_path = os.path.join(result_path,'seventh_heads')
-    save_heads(heads_path,seventh.statistic,seventh.pvalue,sensor_type.lower(),[freqs[fq_ind] for fq_ind in base_fq_indexes]) #conver 'MEG GRAD' to 'grad' and 'MEG MAG' to 'mag'
-    del seventh
 
-def get_tft_data(data,data_type,data_path,sensor_type,freqs,save_tft,load_existing_tft):
-    #This function tries to load tft data if flag @load_existing_tft true and data exists,
-    #if not, it calculates them from @data arg and save them if @save_tft true
-    #@data_type can  be 'target' or 'nontarget'
-    expected_dirname_path = os.path.join(data_path,'%s_BTS_%d_%d_%d_%s.h5' %(sensor_type,freqs[0],freqs[-1],freqs[1] - freqs[0],data_type))
-    if load_existing_tft & os.path.isdir(expected_dirname_path):
-        res=read_large_data(expected_dirname_path)
-    else:
-        res = tft_transofrm(data,freqs) # trials x channels x freqs x times
-        if save_tft:
-            save_large_data(res,expected_dirname_path,freqs) # BTS is a hint for next 3 digits - Bottom,Top,Step
+
+def get_tft_data(data,freqs):
+    #This function just calculate tft transform
+    res = tft_transofrm(data,freqs) # trials x channels x freqs x times
     return res # trials x channels x freqs x times
 
 
 def calc_metrics(data_path,result_path,sensor_type,freqs):
     erase_dir(result_path)
-    data, _ = get_data(data_path,sensor_type) #trials x channels x times
+    data = get_data(data_path,sensor_type) #trials x channels x times
     sensor_type = sensor_type.split(' ')[-1]
 
-    data = get_tft_data(data,'target',data_path,sensor_type,freqs,False,False) # trials x channels x freqs x times
+    data = get_tft_data(data,freqs) # trials x channels x freqs x times
 
     win_lenght = 300 #ms
     left_border = -4300 #ms
     right_border = 200 #ms
+    right_border_ind = right_border - left_border
+
     t_val = []
     p_val = []
-    for ind,t in enumerate(np.arange(left_border,right_border+1,win_lenght)):
-        loop_t_val,loop_p_val = ttest_ind(data[:,:,:,t:t+win_lenght].mean(axis=3),data[:,:,:,t:t+win_lenght].mean(axis=3) - data[:,:,:,t:t+win_lenght].mean(axis=3),axis=0,equal_var=True)
+
+    # np.seterr(all='raise')
+    for ind,t in enumerate(np.arange(0,right_border_ind+1,win_lenght)):
+
+        loop_t_val,loop_p_val = ttest_ind(data[:,:,:,t:t+win_lenght].mean(axis=3),
+                                          (data[:,:,:,right_border_ind:right_border_ind+win_lenght].mean(axis=3) - data[:,:,:,t:t+win_lenght].mean(axis=3)),
+                                          axis=0,equal_var=True)
         t_val.append(loop_t_val)
         p_val.append(loop_p_val)
-        title = '%d_%d' %(t,t+win_lenght)
-        fig = vis_space_freq(t_val,title,freqs)
-        plt.savefig(os.path.join(result_path,title+'_'+sensor_type+'.png'))
-        plt.close(fig)
+        title = '%d_%d' %(t+left_border,t+left_border+win_lenght)
+        save_results(loop_t_val, title, result_path)
 
 
 def erase_dir(path):
